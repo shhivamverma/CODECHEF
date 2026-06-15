90d4b8d6518afdef2f50fd797c9cec44e4ebcbe6
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-15 16:04:59 +0530

    Add files via upload

---
+++ codechef1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class codechef1
+{
+	public static void main (String args[]) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int n=sc.nextInt();
+		    long K=sc.nextLong();
+		    long a[]=new long[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextLong();
+		    }
+		    for(int i=0;i<n;i++)
+		    {
+		        if(a[i]<=K)
+		        {
+		            System.out.print(1);
+		            K=K-a[i];
+		        }
+                else
+                {
+                    System.out.print(0);
+                }
+		    }
+            System.out.println();
+        }
+	}
+}
