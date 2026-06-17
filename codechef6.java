c241e77d4311951db300d5310ba252700bacec81
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-17 22:18:37 +0530

    Update codechef6.java
    Updated comments and clarified problem statement.

--- codechef6.java
+++ codechef6.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class codechef6
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                                    // Problem - Chef Diet
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
