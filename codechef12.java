0dfea6bb8a71a8a3f0a176b1136e5eff34d1439a
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-20 23:24:51 +0530

    Update codechef12.java
    Added a comment to describe the problem being solved.

--- codechef12.java
+++ codechef12.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class codechef12
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                           // Problem -  Janmansh and Assignments
     {
         Scanner sc=new Scanner(System.in);    
         int t=sc.nextInt();
