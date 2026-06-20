21419e0526d01997479197e4299ae0aec8567df0
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-20 23:24:12 +0530

    Add files via upload

---
+++ codechef12.java
@@ -0,0 +1,23 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+import java.util.*;
+public class codechef12
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);    
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int x=sc.nextInt();
+            if(x<=7) 
+            {
+                System.out.println("Yes");
+            } 
+            else 
+            {
+                System.out.println("No");
+            }
+        }
+    }
+}
