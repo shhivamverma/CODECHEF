cacaa44b0fba6ea4dc8b8bd4e3948d2bfe686757
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-20 15:02:34 +0530

    Add files via upload

---
+++ codechef10.java
@@ -0,0 +1,25 @@
+// Once I am done , I will be gone , You won't see me for a while
+
+import java.util.*;
+public class codechef10
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0)
+        {
+            double A=sc.nextDouble();
+            double B=sc.nextDouble();
+            double C=sc.nextDouble();
+            if(((A+B)/2) > C)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
