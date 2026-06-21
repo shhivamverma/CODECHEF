98d1738203bb8cc6a6183005445f9eebd64d1daf
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-21 21:01:48 +0530

    Update codechef13.java
    Added a comment to clarify the problem context.

--- codechef13.java
+++ codechef13.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class codechef13
 {
-    public static void main(String args[])
+    public static void main(String args[])                  // Problem - Exams
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
