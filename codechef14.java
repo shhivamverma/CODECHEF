882cef3aea2a89749b7e2a0c46ee9294ea001d8c
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-21 22:07:11 +0530

    Add files via upload

---
+++ codechef14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class codechef14
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int X=sc.nextInt();
+            int Y=sc.nextInt();
+            int ans=4*X+Y;
+            System.out.println(ans);    
+        } 
+    }
+}
