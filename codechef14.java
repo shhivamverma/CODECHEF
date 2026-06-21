3b0643accce236d94ef42c5998cacabd7cad5efe
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-21 22:08:02 +0530

    Update codechef14.java

--- codechef14.java
+++ codechef14.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codechef14
 {
-    public static void main(String args[])
+    public static void main(String args[])                    // Problem - Chef in his Office
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
