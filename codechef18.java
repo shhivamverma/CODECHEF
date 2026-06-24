2e44f87df7e5810933a63227694befcb963f8c20
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-24 11:28:40 +0530

    Add files via upload

---
+++ codechef18.java
@@ -0,0 +1,33 @@
+// Once I am done  , I will  be  gone , You won't see me for a while.
+
+import java.util.*;
+public class codechef18
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int Xa=sc.nextInt();
+            int Xb=sc.nextInt();
+            int Xc=sc.nextInt();
+            if(Xa>50)
+            {
+                System.out.println("A");
+            }
+            else if(Xb>50)
+            {
+                System.out.println("B");
+            }
+            else if(Xc>50)
+            {
+                System.out.println("C");
+            }
+            else
+            {
+                System.out.println("NOTA");
+            }
+        }
+    }
+}
