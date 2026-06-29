759166a7d0b1e05785b196b3a481277026686f2d
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-29 16:36:21 +0530

    Update codechef30.java
    Removed commented-out code and added a problem statement.

--- codechef30.java
+++ codechef30.java
@@ -10,7 +10,7 @@
 import java.util.*;
 public class codechef30
 {
-    public static void main(String args[])
+    public static void main(String args[])                                       // Problem - Average Permutation
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
