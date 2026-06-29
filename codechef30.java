59ddadc644283a0219539a17fd35e812534c5cc8
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-06-29 16:34:32 +0530

    Add files via upload

---
+++ codechef30.java
@@ -0,0 +1,38 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+//  ███████╗ ██╗  ██╗ ██╗ ██╗   ██╗  █████╗  ███╗   ███╗
+//  ██╔════╝ ██║  ██║ ██║ ██║   ██║ ██╔══██╗ ████╗ ████║
+//  ███████╗ ███████║ ██║ ██║   ██║ ███████║ ██╔████╔██║
+//  ╚════██║ ██╔══██║ ██║ ╚██╗ ██╔╝ ██╔══██║ ██║╚██╔╝██║
+//  ███████║ ██║  ██║ ██║  ╚████╔╝  ██║  ██║ ██║ ╚═╝ ██║
+//  ╚══════╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝  ╚═╝ ╚═╝     ╚═╝
+
+import java.util.*;
+public class codechef30
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int m=n/2;
+            int per[]=new int[n];
+            per[m]=1;
+            per[m-1]=2;
+            for(int i=m;i<n-1;i++)
+            { 
+                per[i+1]=per[i]+2;
+            }
+            for(int i=m-1;i>0;i--)
+            { 
+                per[i-1]=per[i]+2;
+            }
+            for(int i=0;i<n;i++)
+            {
+                System.out.println(per[i]);
+            }
+        }
+    }
+}
