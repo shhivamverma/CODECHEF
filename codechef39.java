08e0757861617ef25ab9d93ae98d5b63751da25a
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-02 21:05:12 +0530

    Add files via upload

---
+++ codechef39.java
@@ -0,0 +1,22 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+import java.util.*;
+class Main 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int x=sc.nextInt();    
+        if(x==a || x==b || x==c) 
+        {
+            System.out.println("Yes");
+        } 
+        else 
+        {
+            System.out.println("No");
+        }
+    }
+}
