ec0a95d05c9b10ae43b916697154529ddce28956
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-05 19:49:39 +0530

    Add files via upload

---
+++ codechef46.java
@@ -0,0 +1,33 @@
+// Once I am done , I will be gone , You won't see me for a while.
+
+//  ███████╗ ██╗  ██╗ ██╗ ██╗   ██╗  █████╗  ███╗   ███╗
+//  ██╔════╝ ██║  ██║ ██║ ██║   ██║ ██╔══██╗ ████╗ ████║
+//  ███████╗ ███████║ ██║ ██║   ██║ ███████║ ██╔████╔██║
+//  ╚════██║ ██╔══██║ ██║ ╚██╗ ██╔╝ ██╔══██║ ██║╚██╔╝██║
+//  ███████║ ██║  ██║ ██║  ╚████╔╝  ██║  ██║ ██║ ╚═╝ ██║
+//  ╚══════╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝  ╚═╝ ╚═╝     ╚═╝
+
+import java.io.*;
+import java.util.*;
+class codechef46
+{
+    public static void main(String args[]) throws Exception 
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0) 
+        {
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            long D=Long.parseLong(st.nextToken());
+            long d=Long.parseLong(st.nextToken());
+            long P=Long.parseLong(st.nextToken());
+            long Q=Long.parseLong(st.nextToken());
+            long k=D/d;
+            long rem=D%d;
+            long ans=d*(k*P+(k*(k-1)/2)*Q)+rem*(P+k*Q);
+            sb.append(ans).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
