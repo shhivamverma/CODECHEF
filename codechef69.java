1372f071ac449b5099cb07ed704272d94197c060
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2026-07-18 17:04:41 +0530

    Update codechef69.java

--- codechef69.java
+++ codechef69.java
@@ -10,7 +10,7 @@
 import java.util.*;
 public class codechef69
 {
-    public static void main(String args[]) 
+    public static void main(String args[])                                    // Problem Equality (1419)
     {
         Scanner sc=new Scanner(System.in);    
         int t=sc.nextInt();
